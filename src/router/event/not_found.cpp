5fd460d05415527b004aa7023d8ee0930225318e
gaowanlu <2209120827@qq.com>
2022-12-21 10:45:07 +0800

    changed:: extern buffer'size

--- src/router/event/not_found.cpp
+++ src/router/event/not_found.cpp
@@ -20,60 +20,74 @@ using namespace colnago::utils;
 
 static void chunk_sender(const shared_ptr<Session> session, int fp)
 {
-    char buffer[20];
-    ssize_t size = read(fp, buffer + 3, 9);
-    if(size==-1){
+    char buffer[512];
+    ssize_t size = read(fp, buffer, 512);
+    if (size == -1)
+    {
         return;
     }
-    buffer[0] = '0' + size;
-    buffer[1] = '\r';
-    buffer[2] = '\n';
-    buffer[3 + size] = '\r';
-    buffer[4 + size] = '\n';
-    buffer[5 + size] = '\0';
+    string str_size;
+    {
+        stringstream s;
+        s << hex;
+        s << size;
+        str_size = s.str();
+    }
     restbed::Bytes bytes;
-    const int len = 5 + size;
-    for(int i=0;i<len;i++){
+    for (char &item : str_size)
+    {
+        bytes.push_back(item);
+    }
+    bytes.push_back('\r');
+    bytes.push_back('\n');
+    for (int i = 0; i < size; i++)
+    {
         bytes.push_back(buffer[i]);
     }
-    // printf("%s", buffer);
-    if(size>0)
+    bytes.push_back('\r');
+    bytes.push_back('\n');
+    if (size > 0)
         session->yield(bytes, [&](const shared_ptr<Session> session) -> void
-        {
-            chunk_sender(session, fp);
-        });
+                       { chunk_sender(session, fp); });
     else
     {
         session->close("0\r\n\r\n");
         close(fp);
-        printf("over\n");
+        //printf("over\n");
     }
 }
 
 static void file_sender(const shared_ptr<Session> session, int fp)
 {
-    char buffer[20];
-    ssize_t size = read(fp, buffer + 3, 9);
-    if(size==-1){
+    char buffer[512];
+    ssize_t size = read(fp, buffer, 512);
+    if (size == -1)
+    {
         return;
     }
-    buffer[0] = '0' + size;
-    buffer[1] = '\r';
-    buffer[2] = '\n';
-    buffer[3 + size] = '\r';
-    buffer[4 + size] = '\n';
-    buffer[5 + size] = '\0';
+    string str_size;
+    {
+        stringstream s;
+        s << hex;
+        s << size;
+        str_size = s.str();
+    }
     restbed::Bytes bytes;
-    const int len = 5 + size;
-    for(int i=0;i<len;i++){
+    for (char &item : str_size)
+    {
+        bytes.push_back(item);
+    }
+    bytes.push_back('\r');
+    bytes.push_back('\n');
+    for (int i = 0; i < size; i++)
+    {
         bytes.push_back(buffer[i]);
     }
-    // printf("%s", buffer);
+    bytes.push_back('\r');
+    bytes.push_back('\n');
     //, {"Content-Disposition", "attchment"}
     session->yield(OK, bytes, {{"Transfer-Encoding", "chunked"}, {"Content-Disposition", "attchment"}}, [&](const shared_ptr<Session> session) -> void
-    { 
-        chunk_sender(session, fp);
-    });
+                   { chunk_sender(session, fp); });
 }
 
 void not_found::event(const shared_ptr<Session> session)
@@ -90,12 +104,12 @@ void not_found::event(const shared_ptr<Session> session)
         int fp = open(file_path.c_str(), ios::in | ios::binary);
         if (fp > 0) // 找到了文件
         {
-            cout << "founded " << file_path << endl;
+            //cout << "founded " << file_path << endl;
             return file_sender(session, fp);
         }
         else
         {
-            cout << "not founded " << file_path << endl;
+            //cout << "not founded " << file_path << endl;
         }
     }
     static std::shared_ptr<std::string> html;
